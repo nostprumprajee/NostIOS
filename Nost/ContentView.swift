b4e608f3b9bb4a5ee25b16598b469f8d131380ea
Wirawat <ibm_wirawat@email.scb.co.th>
2020-11-10 17:32:39 +0700

    initial project

---
+++ Nost/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nost
+//
+//  Created by WirawatBunlom on 10/11/2563 BE.
+//  Copyright © 2563 WirawatBunlom. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
